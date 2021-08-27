f90eded7d589384cc46fc7a9a4a95383a3b487d4
Taynan Vieira <taynan.vieira.one@gmail.com>
2021-08-26 21:40:01 -0300

    (fix) ajuste no secret key

--- src/main/java/com/avanade/dio/jwt/security/SecurityConstants.java
+++ src/main/java/com/avanade/dio/jwt/security/SecurityConstants.java
@@ -2,7 +2,7 @@ package com.avanade.dio.jwt.security;
 
 public class SecurityConstants {
 
-    public static final String SECRET = "sECRETkEYtOgENjwtS";
+    public static final String SECRET = "SecretKeyToGenJWTs";
     public static final long EXPIRATION_TIME = 864_000_000; // 10 days
     public static final String TOKEN_PREFIX = "Bearer";
     public static final String HEADER_STRING = "Authorization";
